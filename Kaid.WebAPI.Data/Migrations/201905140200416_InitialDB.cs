a7ad9627389f0712560fe966c719dcf3d1218d60
KaidDuong <cooleen.cl@gmail.com>
2019-05-14 09:57:26 +0700

    migrations and respositorys

---
+++ Kaid.WebAPI.Data/Migrations/201905140200416_InitialDB.cs
@@ -0,0 +1,318 @@
+﻿namespace Kaid.WebAPI.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Footers",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50),
+                        Content = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.MenuGroups",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Menus",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        URL = c.String(nullable: false, maxLength: 256),
+                        DisplayOrder = c.Int(),
+                        GroupID = c.Int(nullable: false),
+                        Target = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.MenuGroups", t => t.GroupID, cascadeDelete: true)
+                .Index(t => t.GroupID);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        OrderID = c.Int(nullable: false),
+                        ProductID = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.OrderID, t.ProductID })
+                .ForeignKey("dbo.Orders", t => t.OrderID, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .Index(t => t.OrderID)
+                .Index(t => t.ProductID);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CustomerName = c.String(nullable: false, maxLength: 300),
+                        CustomerAddress = c.String(nullable: false, maxLength: 300),
+                        CustomerMoble = c.String(nullable: false, maxLength: 50),
+                        CustomerEmail = c.String(nullable: false, maxLength: 300),
+                        CustomerMessage = c.String(maxLength: 300),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 200),
+                        PaymentMethod = c.String(maxLength: 250),
+                        PaymentStatus = c.String(nullable: false, maxLength: 50),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CategoryID = c.Int(nullable: false),
+                        MoreImages = c.String(storeType: "xml"),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        PromotionPrice = c.Decimal(precision: 18, scale: 2),
+                        Warranty = c.Int(),
+                        Content = c.String(nullable: false),
+                        HotFlag = c.Boolean(),
+                        ViewCount = c.Int(),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Image = c.String(maxLength: 500),
+                        Description = c.String(maxLength: 500),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 256),
+                        UpdateDate = c.DateTime(),
+                        UpdateBy = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                        HomeFlag = c.Boolean(),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.ProductCategories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ParentID = c.Int(),
+                        DisplayOrder = c.Int(),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Image = c.String(maxLength: 500),
+                        Description = c.String(maxLength: 500),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 256),
+                        UpdateDate = c.DateTime(),
+                        UpdateBy = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                        HomeFlag = c.Boolean(),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Pages",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Content = c.String(),
+                        MetaKeyword = c.String(),
+                        MetaDescription = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.PostCategories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ParentID = c.Int(),
+                        DisplayOrder = c.Int(),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Image = c.String(maxLength: 500),
+                        Description = c.String(maxLength: 500),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 256),
+                        UpdateDate = c.DateTime(),
+                        UpdateBy = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                        HomeFlag = c.Boolean(),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Post",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ParentID = c.Int(),
+                        DisplayOrder = c.Int(),
+                        CategoryID = c.Int(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Image = c.String(maxLength: 500),
+                        Description = c.String(maxLength: 500),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 256),
+                        UpdateDate = c.DateTime(),
+                        UpdateBy = c.String(maxLength: 256),
+                        Status = c.Boolean(nullable: false),
+                        HomeFlag = c.Boolean(),
+                        MetaKeyword = c.String(maxLength: 256),
+                        MetaDescription = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.PostCategories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.PostTags",
+                c => new
+                    {
+                        TagID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        PostID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.TagID, t.PostID })
+                .ForeignKey("dbo.Post", t => t.PostID, cascadeDelete: true)
+                .ForeignKey("dbo.Tags", t => t.TagID, cascadeDelete: true)
+                .Index(t => t.TagID)
+                .Index(t => t.PostID);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Type = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ProductTags",
+                c => new
+                    {
+                        TagID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        ProductID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.TagID, t.ProductID })
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .ForeignKey("dbo.Tags", t => t.TagID, cascadeDelete: true)
+                .Index(t => t.TagID)
+                .Index(t => t.ProductID);
+            
+            CreateTable(
+                "dbo.Slides",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Alias = c.String(nullable: false, maxLength: 256, unicode: false),
+                        Image = c.String(maxLength: 256),
+                        Description = c.String(),
+                        URL = c.String(maxLength: 256),
+                        DisplayOrder = c.Int(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.SupportOnlines",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Department = c.String(maxLength: 50),
+                        Skype = c.String(maxLength: 50),
+                        Moble = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 50),
+                        Yahoo = c.String(maxLength: 50),
+                        Facebook = c.String(maxLength: 50),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.SystemConfigs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Code = c.String(nullable: false, maxLength: 50, unicode: false),
+                        ValueString = c.String(maxLength: 50),
+                        ValueInt = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.VisitorStatisics",
+                c => new
+                    {
+                        ID = c.Guid(nullable: false),
+                        VisitedDate = c.DateTime(nullable: false),
+                        IPAddress = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProductTags", "TagID", "dbo.Tags");
+            DropForeignKey("dbo.ProductTags", "ProductID", "dbo.Products");
+            DropForeignKey("dbo.PostTags", "TagID", "dbo.Tags");
+            DropForeignKey("dbo.PostTags", "PostID", "dbo.Post");
+            DropForeignKey("dbo.Post", "CategoryID", "dbo.PostCategories");
+            DropForeignKey("dbo.OrderDetails", "ProductID", "dbo.Products");
+            DropForeignKey("dbo.Products", "CategoryID", "dbo.ProductCategories");
+            DropForeignKey("dbo.OrderDetails", "OrderID", "dbo.Orders");
+            DropForeignKey("dbo.Menus", "GroupID", "dbo.MenuGroups");
+            DropIndex("dbo.ProductTags", new[] { "ProductID" });
+            DropIndex("dbo.ProductTags", new[] { "TagID" });
+            DropIndex("dbo.PostTags", new[] { "PostID" });
+            DropIndex("dbo.PostTags", new[] { "TagID" });
+            DropIndex("dbo.Post", new[] { "CategoryID" });
+            DropIndex("dbo.Products", new[] { "CategoryID" });
+            DropIndex("dbo.OrderDetails", new[] { "ProductID" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderID" });
+            DropIndex("dbo.Menus", new[] { "GroupID" });
+            DropTable("dbo.VisitorStatisics");
+            DropTable("dbo.SystemConfigs");
+            DropTable("dbo.SupportOnlines");
+            DropTable("dbo.Slides");
+            DropTable("dbo.ProductTags");
+            DropTable("dbo.Tags");
+            DropTable("dbo.PostTags");
+            DropTable("dbo.Post");
+            DropTable("dbo.PostCategories");
+            DropTable("dbo.Pages");
+            DropTable("dbo.ProductCategories");
+            DropTable("dbo.Products");
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.Menus");
+            DropTable("dbo.MenuGroups");
+            DropTable("dbo.Footers");
+        }
+    }
+}
---
+++ Kaid.WebAPI.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Kaid.WebAPI.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Kaid.WebAPI.Data.KaidDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Kaid.WebAPI.Data.KaidDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
