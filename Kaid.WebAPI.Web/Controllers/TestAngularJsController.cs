27c64c6eb20f990f74fd1e1271850d4cf1833a1b
KaidDuong <cooleen.cl@gmail.com>
2019-05-21 14:29:54 +0700

    introduce for AngularJs

---
+++ Kaid.WebAPI.Web/Controllers/TestAngularJsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kaid.WebAPI.Web.Controllers
+{
+    public class TestAngularJsController : Controller
+    {
+        // GET: TestAngularJs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
