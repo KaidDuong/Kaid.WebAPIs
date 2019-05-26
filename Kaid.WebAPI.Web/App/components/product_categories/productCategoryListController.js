d9b115bfb3ed32bc0770a38beb3c2d1c9cdfba71
KaidDuong <cooleen.cl@gmail.com>
2019-05-26 18:44:18 +0700

    add notification service for project use Toastr's library

--- Kaid.WebAPI.Web/App/components/product_categories/productCategoryListController.js
+++ Kaid.WebAPI.Web/App/components/product_categories/productCategoryListController.js
@@ -2,21 +2,20 @@
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope','apiService'];
+    productCategoryListController.$inject = ['$scope','apiService','notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService,notificationService) {
 
         $scope.productCategories = [];
         $scope.page = 0;
-        $scope.pageCount = 0;
+        //$scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
         $scope.keyword = '';
 
         $scope.search = function () {
             getProductCategories()
         };
-
-        function search() {getProductCategories()}
+        
         function getProductCategories(page) {
             page = page || 0;
             var config = {
@@ -30,6 +29,12 @@
                             config,
                             function (result)
                             {
+                                if (result.data.TotalCount == 0) {
+                                    notificationService.displayWarning('There are no the records was found!');
+                                }
+                                else {
+                                    notificationService.displaySuccess('Find ' + result.data.TotalCount + ' record(s)');
+                                }
                                 $scope.productCategories = result.data.Items;
                                 $scope.page = result.data.Page;
                                 $scope.pagesCount = result.data.TotalPages;
