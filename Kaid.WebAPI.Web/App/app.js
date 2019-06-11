fdf204d5b606d527960c7cd8b49e91f5b74ae148
KaidDuong <cooleen.cl@gmail.com>
2019-06-11 17:31:58 +0700

    login form

--- Kaid.WebAPI.Web/App/app.js
+++ Kaid.WebAPI.Web/App/app.js
@@ -15,14 +15,26 @@
                      ];
 
     function config($stateProvider,
-                    $urlRouterProvider)
-    {
-        $stateProvider.state('home', {
-            url: "/admin",
-            templateUrl: "/app/components/home/homeView.html",
-            controller: "homeController"
-        });
+        $urlRouterProvider) {
+        $stateProvider.state('base',
+            {
+                url: '',
+                templateUrl: '/app/shared/views/baseView.html',
+                abstract: true
+            })
+            .state('login',
+            {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent: 'base',
+                templateUrl: "/app/components/home/homeView.html",
+                controller: "homeController"
+            });
 
-        $urlRouterProvider.otherwise('/admin');
+        $urlRouterProvider.otherwise('/login');
     }
 })();
