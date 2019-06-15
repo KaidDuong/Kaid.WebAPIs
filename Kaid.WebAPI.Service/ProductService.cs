7f5f48dd008f4d5e8564508de0edbce35115c1c9
KaidDuong <cooleen.cl@gmail.com>
2019-06-15 23:28:29 +0700

    create pages for products, using ckeditor

---
+++ Kaid.WebAPI.Service/ProductService.cs
@@ -0,0 +1,82 @@
+﻿using Kaid.WebAPI.Data.Infrastructure;
+using Kaid.WebAPI.Data.Repositories;
+using Kaid.WebAPI.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaid.WebAPI.Service
+{
+    public interface IProductService
+    {
+        Product Add(Product product);
+
+        void Update(Product product);
+
+        Product Delete(int id);
+
+        IEnumerable<Product> GetAll();
+        IEnumerable<Product> GetAll(string keyword);
+        IEnumerable<Product> GetAllByCategoryId(int parentId);
+
+        Product GetById(int id);
+
+        void SaveChanges();
+    }
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepositories;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            this._productRepositories = productRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public Product Add(Product product)
+        {
+           return  _productRepositories.Add(product);
+            
+        }
+
+        public Product Delete(int id)
+        {
+            return _productRepositories.Delete(id);
+            
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepositories.GetAll();
+        }
+
+        public IEnumerable<Product> GetAll(string keyword)
+        {
+            return (!string.IsNullOrEmpty(keyword)) ?
+                   _productRepositories.GetMulti(k => k.Name.Contains(keyword) || k.Content.Contains(keyword) || k.Description.Contains(keyword))
+                   :_productRepositories.GetAll();
+        }
+
+        public IEnumerable<Product> GetAllByCategoryId(int CategoryId)
+        {
+            return _productRepositories.GetMulti(k => k.Status && k.CategoryID == CategoryId);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productRepositories.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Product product)
+        {
+            _productRepositories.Update(product);
+        }
+    }
+}
---
+++ Kaid.WebAPI.Web/Api/ProductController.cs
@@ -0,0 +1,181 @@
+﻿using AutoMapper;
+using Kaid.WebAPI.Model.Models;
+using Kaid.WebAPI.Service;
+using Kaid.WebAPI.Web.Infrastructure.Core;
+using Kaid.WebAPI.Web.Infrastructure.Extentions;
+using Kaid.WebAPI.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace Kaid.WebAPI.Web.Api
+{
+    [RoutePrefix("api/product")]
+    [Authorize]
+    public class ProductController : ApiControllerBase
+    {
+        private IProductService _productService;
+       
+        public ProductController(IProductService productService,IErrorService errorService): base(errorService)
+        {
+            this._productService = productService;
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public HttpResponseMessage GetAll(HttpRequestMessage requestMessages,
+            string keyword, int page, int pageSize=20)
+        {
+            return CreateHttpResponse(requestMessages, 
+                                      () => {
+                                          var model = _productService.GetAll(keyword);
+
+                                          var totalRow = model.Count();
+
+                                          var query = model.OrderByDescending(k => k.CreateDate)
+                                                          .Skip(page * pageSize)
+                                                          .Take(pageSize);
+
+                                          var responseData = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(query);
+
+                                          PaginationSet<ProductViewModel> paginationSet = new PaginationSet<ProductViewModel>()
+                                          {
+                                              Items = responseData,
+                                              Page = page,
+                                              TotalPages = (int)Math.Ceiling((decimal)(totalRow / pageSize)),
+                                              TotalCount = totalRow
+                                          };
+
+                                          return requestMessages.CreateResponse(HttpStatusCode.OK, paginationSet);
+                                      }
+                                      );
+        }
+
+        [HttpGet]
+        [Route("getbyid")]
+        public HttpResponseMessage GetById(HttpRequestMessage requestMessage, int id)
+        {
+            return CreateHttpResponse(requestMessage,
+                                      () =>
+                                      {
+                                          var model = _productService.GetById(id);
+
+                                          var responseData = Mapper.Map<Product, ProductViewModel>(model);
+
+                                          return requestMessage.CreateResponse(HttpStatusCode.OK, responseData);
+                                      }
+                                      );
+
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public HttpResponseMessage Create(HttpRequestMessage requestMessage, ProductViewModel viewModel)
+        {
+            return CreateHttpResponse(requestMessage,
+                                      () => {
+                                          if (!ModelState.IsValid)
+                                          {
+                                              return requestMessage.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                                          }
+                                          else
+                                          {
+                                              var model = new Product();
+
+                                              model.UpdateProduct(viewModel);
+
+                                              model=_productService.Add(model);
+                                              _productService.SaveChanges();
+
+                                              var responseData = Mapper.Map<Product, ProductViewModel>(model);
+
+                                              return requestMessage.CreateResponse(HttpStatusCode.Created, responseData);
+                                          }
+                                      });
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public HttpResponseMessage Update(HttpRequestMessage requestMessage, ProductViewModel viewModel)
+        {
+            return CreateHttpResponse(requestMessage, 
+                                      () => {
+                                          if (!ModelState.IsValid)
+                                          {
+                                              return requestMessage.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                                          }
+                                          else
+                                          {
+                                              var model = new Product();
+
+                                              model.UpdateProduct(viewModel);
+
+                                              model.UpdateDate = DateTime.Now;
+                                              model.UpdateBy = User.Identity.Name;
+
+                                              _productService.Update(model);
+                                              _productService.SaveChanges();
+
+                                              var responseData = viewModel;
+
+                                              return requestMessage.CreateResponse(HttpStatusCode.Created, responseData);
+                                          }
+                                      });
+        }
+
+        [HttpDelete]
+        [Route("remove")]
+        public HttpResponseMessage Remove(HttpRequestMessage requestMessage, int id)
+        {
+            return CreateHttpResponse(requestMessage,
+                                      () => {
+                                          if (!ModelState.IsValid)
+                                          {
+                                              return requestMessage.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                                          }
+                                          else{
+                                              var model = _productService.Delete(id);
+                                              _productService.SaveChanges();
+
+                                              var responseData = Mapper.Map<Product, ProductViewModel>(model);
+
+                                              return requestMessage.CreateResponse(HttpStatusCode.OK, responseData);
+                                          }
+                                      });
+        }
+
+        [HttpDelete]
+        [Route("removes")]
+        public HttpResponseMessage Removes(HttpRequestMessage requestMessage, string listIds)
+        {
+            return CreateHttpResponse(requestMessage, 
+                                      () => {
+                                          if (!ModelState.IsValid)
+                                          {
+                                              return requestMessage.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                                          }
+                                          else
+                                          {
+                                              var ids = new JavaScriptSerializer().Deserialize<List<int>>(listIds);
+
+                                              var models = new List<Product>();
+
+                                              foreach( var id in ids)
+                                              {
+                                                  models.Add(_productService.Delete(id));
+                                              }
+
+                                              _productService.SaveChanges();
+
+                                              var responseData = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(models);
+
+                                              return requestMessage.CreateResponse(HttpStatusCode.OK, responseData);
+                                          }
+                                      });
+        }
+    }
+}
